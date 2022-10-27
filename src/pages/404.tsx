80d18a5b508025921ef240d5048a48437b27b886
Igor Araujo <igor@Igor>
2022-10-26 21:42:34 -0300

    add: 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+const NotFound = () => {
+  return (
+    <Flex h="100vh" align="center" justify="center">
+      <Text>Página não encontrada</Text>
+    </Flex>
+  );
+};
+
+export default NotFound;
