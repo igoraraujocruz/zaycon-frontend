621fba79af193ce2abb581f82e0b0d30a3a0627b
Igor <igoraraujocruzz@gmail.com>
2023-01-04 12:31:35 -0300

    changes!

--- src/services/hooks/useChats.tsx
+++ src/services/hooks/useChats.tsx
@@ -1,6 +1,5 @@
 import { useQuery } from 'react-query';
 import { api } from '../apiClient';
-import { queryClient } from '../queryClient';
 
 interface Account {
   _id: string;
@@ -19,10 +18,10 @@ interface NewAccount {
 }
 
 interface Message {
-    _id: string;
-    accountId: string;
-    message: string;
-    createdAt: string;
+  _id: string;
+  accountId: string;
+  message: string;
+  createdAt: string;
 }
 
 export const getAccounts = async (): Promise<NewAccount[]> => {
@@ -51,29 +50,35 @@ export function useAccount() {
   return useQuery(['account'], () => getAccounts());
 }
 
-export const getChatByAccount = async (accountId: string): Promise<Message[]> => {
-    if (accountId) {
-      const { data } = await api.get(`/clients/chatByAccount?account=${accountId}`);
+export const getChatByAccount = async (
+  accountId: string,
+): Promise<Message[]> => {
+  if (accountId) {
+    const { data } = await api.get(
+      `/clients/chatByAccount?account=${accountId}`,
+    );
 
-      const chat = data.map((message: Message) => {
-        return {
-          id: message._id,
-          accountId: message.accountId,
-          message: message.message,
-          createdAt: new Date(message.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: '2-digit',
-            year: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-          }),
-        };
-      });
-  
-      return chat;
-    }
-  };
-  
-  export function useChatByAccount(accountId: string) {
-    return useQuery(['ChatByAccount', accountId], () => getChatByAccount(accountId));
+    const chat = data.map((message: Message) => {
+      return {
+        id: message._id,
+        accountId: message.accountId,
+        message: message.message,
+        createdAt: new Date(message.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: '2-digit',
+          year: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+      };
+    });
+
+    return chat;
   }
+};
+
+export function useChatByAccount(accountId: string) {
+  return useQuery(['ChatByAccount', accountId], () =>
+    getChatByAccount(accountId),
+  );
+}
