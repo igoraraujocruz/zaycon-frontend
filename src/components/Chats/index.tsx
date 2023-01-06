334d8893b513667dd1281c717fb08c1b66cb8136
Igor <igoraraujocruzz@gmail.com>
2023-01-06 16:43:40 -0300

    changes

--- src/components/Chats/index.tsx
+++ src/components/Chats/index.tsx
@@ -19,6 +19,8 @@ import { queryClient } from '../../services/queryClient';
 
 console.log(process.env.NEXT_PUBLIC_INSTAGRAM_TOKEN);
 
+console.log(process.env.INSTAGRAM_TOKEN);
+
 type MessageValidate = {
   message: string;
 };
